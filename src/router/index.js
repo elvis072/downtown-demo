d42609b8e95ff742cdece9f55de33d2fb7c33ad3
Elvis Antigua <elvis.antigua@jobsity.com>
2025-05-27 00:27:43 -0400

    Fix routing for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -55,8 +55,15 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router 
